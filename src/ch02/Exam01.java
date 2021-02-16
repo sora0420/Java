c2049b7b0a5dd5eff68107f9966eaed1353359e3
sora0420 <thfk0420@naver.com>
2021-02-16 15:34:59 +0900

    test
    Change-Id: Idf7b6921a606815e1e45aa239d2ca9b2391e99ad

---
+++ src/ch02/Exam01.java
@@ -0,0 +1,19 @@
+package ch02;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte var1 = 1;
+		short var2 = 1;
+		int var3 = 1;
+		long var = 1;
+		
+		float var5 = 2.5f;
+		double var6 = 2.5;
+		
+		boolean var7 = true;
+		boolean var8 = false;
+	}
+
+}
