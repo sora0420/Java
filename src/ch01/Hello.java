0fb162e5a19712954b6c88d92ae561525ba64d0e
sora0420 <thfk0420@naver.com>
2021-02-16 15:21:52 +0900

    dddddd
    Change-Id: If3a3323c00ee09bef9242517afe8e18ed60724d9

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Javaa!");
+		System.out.println("Hello Javaaaaa!");
 		System.out.println("Hello Java!");
 		System.out.println("Hello Java!");
 	}
