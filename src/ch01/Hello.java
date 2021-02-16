8eedcb73497d02523dcb32d32f019c573c0d60e4
sora0420 <thfk0420@naver.com>
2021-02-16 14:39:12 +0900

    test

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java!");
-		System.out.println("Hello Java!d");
+		System.out.println("Hello Java!");
 		System.out.println("Hello Java!");
 
 	}
